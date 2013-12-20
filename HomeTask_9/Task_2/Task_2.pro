bdd89cf312b00f95bd5df09eddb1f680eb9eea6b
maxim-yakupov <maximkaya1995@gmail.com>
2013-12-21 01:46:45 +0400

    added second task in homework #9

--- HomeTask_9/Task_2/Task_2.pro
+++ HomeTask_9/Task_2/Task_2.pro
@@ -3,5 +3,7 @@ CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
 
+QMAKE_CXXFLAGS += -std=c++11
+
 SOURCES += main.cpp
 
--- HomeTask_9/Task_2/main.cpp
+++ HomeTask_9/Task_2/main.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
+const int codeShift = 31;
+const int mult = 23;
+
 int charStringLength(const char str[]);
+long int pow(int base, int exp);
+long int hashStr(char* str, unsigned int length);
+bool compare(char* str1, char* str2);
 
 int main()
 {
@@ -12,11 +18,26 @@ int main()
     cin.getline(str1, 500);
     cout << "Write string for search: ";
     cin.getline(str2, 500);
-    int lengthStr1 = charStringLength(str1);
-    int lengthStr2 = charStringLength(str2);
+    unsigned int lengthStr1 = charStringLength(str1);
+    unsigned int lengthStr2 = charStringLength(str2);
     long int hashForSearch = hashStr(str2, lengthStr2);
+    long int hashCurrent = hashStr(str1, lengthStr2);
 
+    long int multEdge = pow(mult, lengthStr2 - 1);
+    unsigned int pos = lengthStr2 - 1;
+    do
+    {
+        pos++;
+        if (hashCurrent == hashForSearch)
+        {
+            cout << "hash match from position = " << pos - lengthStr2 << endl;
+            cout << (compare(str2, &str1[pos - lengthStr2]) ? "FOUND!" : "false alarm(collision)") << endl;
+        }
+        hashCurrent = (-(str1[pos - lengthStr2] - codeShift) * multEdge + hashCurrent) * mult + str1[pos] - codeShift;
+    } while (pos < lengthStr1);
 
+    delete [] str1;
+    delete [] str2;
     return 0;
 }
 
@@ -29,12 +50,35 @@ int charStringLength(const char str[])
     }
     return counter;
 }
-long int hashChar(char ch)
-{
 
+bool compare(char* str1, char* str2)
+{
+    while (*str1 && (*str1 == *str2))
+    {
+        str1++;
+        str2++;
+    }
+    return !*str1;
 }
 
-long int hashStr(char* str, int length)
+long int hashStr(char* str, unsigned int length)
 {
+    long int hash = str[0] - codeShift;
+    unsigned int pos = 1;
+    while (pos < length)
+    {
+        hash = (hash * mult + str[pos] - codeShift);
+        pos++;
+    }
+    return hash;
+}
 
+long int pow(int base, int exp)
+{
+    long int answer = base;
+    while (--exp)
+    {
+        answer *=base;
+    }
+    return answer;
 }
