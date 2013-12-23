6701c97e4f71e5c4edca94ec71f6e33d289e821e
Maxim Yakupov <maxim-yakupov@users.noreply.github.com>
2013-12-23 18:10:30 +0300

    deleted extra spaces

--- HomeTask_12/Task_1/main.cpp
+++ HomeTask_12/Task_1/main.cpp
@@ -12,8 +12,7 @@ bool isCompositionSign(char ch)
 
 bool isCompositionToken(char*& str)
 {
-    return (isNumberWithFloatPoint(str) || ((*str == '(') && isArifmExpression(++str)
-                                            && (*(str++) == ')')));
+    return (isNumberWithFloatPoint(str) || ((*str == '(') && isArifmExpression(++str) && (*(str++) == ')')));
 }
 
 bool isComposition(char*& str)
