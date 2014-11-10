2edeae4bb6c3e55f2a54cbfc5b50dea83aa13e91
maxim-yakupov <maximkaya1995@gmail.com>
2014-11-10 10:33:08 +0300

    task 5: done

---
+++ 5/yakupov/Main.java
@@ -0,0 +1,56 @@
+package yakupov;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static long timer(Matrix m1, Matrix m2) {
+        long st, en;
+        st = System.nanoTime();
+        Matrix.multi(m1, m2);//----
+        en = System.nanoTime();
+        return en-st;
+    }
+
+    public static void writeMatrix(Matrix m, PrintWriter myPrintWriter) {
+        for (int i = 0; i < m.getH(); i++) {
+            for (int j = 0; j < m.getW(); j++) {
+                myPrintWriter.print(m.get(i, j) + " ");
+            }
+            myPrintWriter.println();
+        }
+        myPrintWriter.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner myScanner = new Scanner(new File("input.txt"));
+        PrintWriter myPrintWriter = new PrintWriter(new File("output.txt"));
+
+        int n = myScanner.nextInt();
+        Matrix m1 = new Matrix(n, n);
+        Matrix m2 = new Matrix(n, n);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++){
+                m1.set(i, j, myScanner.nextDouble());
+            }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++){
+                m2.set(i, j, myScanner.nextDouble());
+            }
+        System.out.println("First run: " + timer(m1, m2) + "ns");
+
+        writeMatrix(Matrix.multi(m1, m2), myPrintWriter);
+
+        m1.tranponent();
+        m2.tranponent();
+
+        System.out.println("Second run: " + timer(m1, m2) + "ns");
+
+        writeMatrix(Matrix.multi(m1, m2), myPrintWriter);
+
+        myPrintWriter.close();
+    }
+}
---
+++ 5/yakupov/Matrix.java
@@ -0,0 +1,106 @@
+package yakupov;
+
+/**
+ * Class of Matrix
+ */
+public class Matrix {
+    private int h, w;
+    private double[] a = null;
+
+    /**
+     * Constructor
+     * @param h Number of rows
+     * @param w Number of columns
+     */
+    Matrix(int h, int w) {
+        this.h = h;
+        this.w = w;
+        a = new double[h*w];
+        for (int i = 0; i < h*w; i++) {
+            a[i] = 0;
+        }
+    }
+
+    /**
+     * Get number of rows
+     * @return Number of rows
+     */
+    public int getH() {
+        return h;
+    }
+
+    /**
+     * Get number of columns
+     * @return Number of columns
+     */
+    public int getW() {
+        return w;
+    }
+
+    /**
+     * Calculates coords into position in array
+     * @param i First coord
+     * @param j Second coord
+     * @return Position in array
+     */
+    private int ind(int i, int j) {
+        return i * getW() + j;
+    }
+
+    /**
+     * Gets value by coords
+     * @param i First coord
+     * @param j Second coord
+     * @return value
+     */
+    public double get(int i, int j) {
+        return a[ind(i, j)];
+    }
+
+    /**
+     * Sets value by coords
+     * @param i First coord
+     * @param j Second coord
+     * @param value Value
+     */
+    public void set(int i, int j, double value) {
+        a[ind(i, j)] = value;
+    }
+
+    /**
+     * Multiply two matrixes(static member)
+     * @param m1 First matrix
+     * @param m2 Second matrix
+     * @return Result matrix
+     */
+    public static Matrix multi(Matrix m1, Matrix m2) {
+        int hRes = m1.getH();
+        int wRes = m2.getW();
+
+        if (m1.getW() != m2.getH()) {
+            System.err.println("Can't do multiplication cause of bad sizes");
+            return new Matrix(1, 1);
+        }
+
+        int count = m1.getW();
+        Matrix res = new Matrix(hRes, wRes);
+        for (int i = 0; i < hRes; i++)
+            for (int j = 0; j < wRes; j++)
+                for (int k = 0; k < count; k++) {
+                    res.set(i, j, res.get(i, j) + m1.get(i, k) * m2.get(k, j));
+                }
+        return res;
+    }
+
+    /**
+     * Transponents matrix
+     */
+    public void tranponent() {
+        double[] T = new double[getH() * getW()];
+        for (int i = 0; i < getH(); i++)
+            for (int j = 0; j < getW(); j++) {
+                T[ind(j, i)] = a[ind(i, j)];
+            }
+        a = T;
+    }
+}
