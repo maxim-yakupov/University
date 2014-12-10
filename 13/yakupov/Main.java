f718fa6c59b2486108e2e63806da4be130d9d8dc
maxim-yakupov <maximkaya1995@gmail.com>
2014-12-10 08:23:37 +0300

    task 13: added SGraph class based on HashSet

--- 13/yakupov/Main.java
+++ 13/yakupov/Main.java
@@ -31,6 +31,34 @@ public class Main {
             );
         }
         System.out.println("After changing :  " + graph.toString());
+
+        System.out.println("---------------------SGRAPH");/////////////////////////////////////////////////
+
+        SGraph sgraph = new SGraph();
+
+        str = ("1-2 2-1 2-3 3-2 2-4 4-2 2-5 5-2 3-4 4-3 4-5 5-4 5-6 6-5").split(" ");
+        System.out.print("Input string:");
+        for (String s : str)
+        {
+            System.out.print(" " + s);
+            String[] stt = s.split("-");
+            int v1 = Integer.parseInt(stt[0]);
+            int v2 = Integer.parseInt(stt[1]);
+            sgraph.add(v1, v2);
+        }
+        System.out.println();
+
+        System.out.println("Before changing:" + sgraph.toString());
+        forDel = ("1-2 2-3 3-2 2-5 5-2 3-4 5-4 5-6 6-5").split(" ");
+        for (String s : forDel) {
+            String[] stt = s.split("-");
+            int v1 = Integer.parseInt(stt[0]);
+            int v2 = Integer.parseInt(stt[1]);
+            System.out.println("Trying to remove " + v1 + "-" + v2 + ": "
+                            + (sgraph.remove(v1, v2) ? "success" : "not in graph")
+            );
+        }
+        System.out.println("After changing :  " + sgraph.toString());
     }
 
     private static void makeTests() {
@@ -44,6 +72,11 @@ public class Main {
             System.out.println(test(test) ? "Passed." : "Failed. test str: '" + test + "'");
         }
         System.out.println("-----END-OF-TESTS-----");
+        System.out.println("--------TESTS--------SGRAPH");
+        for (String test : tests) {
+            System.out.println(testSGraph(test) ? "Passed." : "Failed. test str: '" + test + "'");
+        }
+        System.out.println("-----END-OF-TESTS----SGRAPH");
     }
 
     public static boolean test(String testData) {
@@ -67,6 +100,29 @@ public class Main {
             if (!graph.contains(e)) {
                 return false;
             }
+        }
+        return true;
+    }
+
+    public static boolean testSGraph(String testData) {
+        SGraph graph = new SGraph();
+        HashSet<Edge> set = new HashSet<Edge>();
+        String[] str = testData.split(" ");
+        for (String s : str)
+        {
+            String[] stt = s.split("-");
+            int v1 = Integer.parseInt(stt[0]);
+            int v2 = Integer.parseInt(stt[1]);
+            graph.add(v1, v2);
+            Edge fEdge = new Edge(v1, v2);
+            if (!fEdge.equals(new Edge(v2, v1))) {
+                return false;
+            }
+            set.add(fEdge);
+        }
+
+        if (!set.equals(graph.edges)) {
+            return false;
         }
         return true;
     }
